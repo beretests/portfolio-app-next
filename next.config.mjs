ed1ada1c7fe9623c401ac6e7da3abb0e1e63ead5
Eberechi Omeje <eb.omeje@gmail.com>
2024-11-16 14:10:17 -0600

    more features I lost track

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig = {
   output: "export",
@@ -12,5 +13,9 @@ const nextConfig = {
   },
   missingSuspenseWithCSRBailout: false,
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
