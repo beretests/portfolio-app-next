9abf6aee92a74982a526ffa20ebbd74ade7a7e9e
Eberechi Omeje <eb.omeje@gmail.com>
2026-07-14 04:36:11 -0600

    Add files via upload

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -6,39 +6,35 @@ import dynamic from "next/dynamic";
 import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import SliderArrow from "./SliderArrow";
 
 const SlickSlider = dynamic(() => import("react-slick"), { ssr: false });
 
 interface ImageSliderProps {
   images: string[];
-  onImageClick?: () => void;
-  isZoomed?: boolean;
 }
 
-const ImageSlider: React.FC<ImageSliderProps> = ({
-  images,
-  onImageClick,
-  isZoomed,
-}) => {
+const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+  const showArrows = images.length > 1;
   const settings = {
-    dots: true,
-    infinite: true,
+    dots: false,
+    infinite: showArrows,
     speed: 750,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    arrows: showArrows,
+    autoplay: false,
+    nextArrow: <SliderArrow direction="next" />,
+    prevArrow: <SliderArrow direction="prev" />,
   };
 
   return (
-    <div className="h-full [&_.slick-dots]:!-bottom-4 [&_.slick-dots>li>button:before]:text-xs [&_.slick-dots>li>button:before]:text-foreground/70">
+    <div className="h-full" onClick={(event) => event.stopPropagation()}>
       <SlickSlider {...settings}>
         {images.map((image, index) => (
           <div
             key={index}
-            onClick={onImageClick}
-            className="cursor-pointer h-full flex items-center justify-center"
+            className="h-full flex items-center justify-center"
           >
             <div
               className="relative w-full pb-8"
